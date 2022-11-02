14a26e5fdc6e5ef8ea591a79ccf079fdc040343e
jack <doemjack@gmail.com>
2022-11-02 16:49:16 -0500

    nov1 class project

---
+++ index.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import { collectDefaultMetrics, register, Counter } from 'prom-client';
+
+collectDefaultMetrics();
+
+const app = express();
+
+const counter = new Counter({
+  name: 'HelloRouteCount',
+  help: 'metric_help',
+});
+
+app.get('/', (req, res) => {
+  counter.inc();
+  res.send('Hello Prom!')
+})
+
+app.get('/metrics', async (_req, res) => {
+	try {
+		res.set('Content-Type', register.contentType);
+		res.end(await register.metrics());
+		counter.inc();
+	} catch (err) {
+		res.status(500).end(err);
+	}
+});
+
+app.listen(4001, '0.0.0.0');
+
+console.log('Server is running...');
